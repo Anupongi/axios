398f01548feaad0051280f0fd8f20941ec89f7cf
Icesolo <jmfairy123@hotmail.com>
2019-04-30 10:48:30 +0700

    update last

--- src/components/igpic.vue
+++ src/components/igpic.vue
@@ -1,12 +1,19 @@
 <template>
     <div id="igpic">
-        <input type="text" v-model="keyword"> <button type="submit" class="button"><i class="fas fa-search"></i> ค้นหา</button>
+        <input type="text" v-model="keyword"> <button type="button" @click="keyword" class="button">ค้นหา</button>
         <h3 v-for="h in hashtag" :key="h.id">{{'#'+ h.name}}</h3>
-        <ul class="pic_all"> 
-            <li v-for="c in ig" :key="c.id" style="list-style-type: none;  ">
-                <img class="pic" :src="c.node.display_url">
-            </li>
-        </ul>
+              <!--<div v-for="c in ig" :key="c.id" style="list-style-type: none;  ">
+                <img class="pic" :src="c.node.display_url"> 
+            </div> -->  
+            <div class="container"  v-for="c in ig" :key="c.id">
+                <img :src="c.node.display_url" class="image">
+                <div class="overlay">
+                    <div class="text"  >
+                       <small><i class="fas fa-heart"></i>  &nbsp; <i class="fas fa-comment-alt"></i></small>
+                    </div>
+                </div>
+            </div>
+        
     </div>
 </template>
 
@@ -18,6 +25,7 @@ export default {
       return{
           ig: [],
           hashtag: []  ,
+          like:[],
           keyword:""
       }  
     },
@@ -30,8 +38,14 @@ export default {
             )
             .then(result =>{
                 this.ig = result.data.graphql.hashtag.edge_hashtag_to_media.edges;
-                this.hashtag = result.data.graphql;    
-            })
+                this.hashtag = result.data.graphql;
+                this.like = result.data.graphql.hashtag.edge_hashtag_to_media.edges;
+                   
+            }).each(function(index, value) {		
+	            $('#igpic container').append('<p> '+value.node.edge_media_to_caption.edges[0].node.text+'</p><small><i class="fas fa-heart"></i>  '+value.node.edge_liked_by.count+'&nbsp; <i class="fas fa-comment-alt"></i>  '+value.node.edge_media_to_comment.count+'</small>');
+	      		return index<100-1;
+			})
+
             .catch(error => {
                 console.log(error);
             });
@@ -72,18 +86,48 @@ export default {
     h3{
         font-size: 30px;
     }
-    .pic_all {
-        margin: 0; 
-        padding: 0;
-        .pic{
-            width: 200px;
-            height: 250px;
-            width: 25%;
+    
+        .container {
             margin: 1.5%;
-            padding-left:  50px;
+            padding: 10;
             float: left;
             position: relative;
-        }   
+            width: 30%;
+            
+            .image {
+                display: block;
+                width: 100%;
+                height: 250px;
+                
+                
+            }
+            .overlay {
+                transition: .5s ease;
+                opacity: 0;
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                background-color: #008CBA;
+                height: 100%;
+  width: 100%;
+            
+            }
+            .overlay:hover {
+                opacity: 1;
+            }
+            .text {
+                color: black;
+                font-size: 20px;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                -webkit-transform: translate(-50%, -50%);
+                -ms-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%);
+                text-align: center;
+            } 
     }
 }
 </style>
